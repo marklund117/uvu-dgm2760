b235d9e0bc6dab4f84af2eea5f59d69d4ea6a1e2
Mark Lund <marklund117@gmail.com>
2022-03-10 10:27:11 -0700

    setting up template

---
+++ 04fortune/js/main.js
@@ -0,0 +1,6 @@
+// this is the main JS file for module 4 in DGM 2760 2022 spring, mark lund
+
+// fill text of header h1
+document.querySelector('header > h1').innerText = "Fortune Teller"
+// fill text of header h2
+document.querySelector('header > h2').innerText = "Switch Statements"
