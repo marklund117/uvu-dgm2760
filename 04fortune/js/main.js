06538e2420de0e4368aea1379d0b65e8b395a10d
Mark Lund <marklund117@gmail.com>
2022-03-11 13:55:04 -0700

    this should be done

--- 04fortune/js/main.js
+++ 04fortune/js/main.js
@@ -15,14 +15,11 @@ function getRandomIntInRange(minInt, maxInt) {
     return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt
 }
 
-// get a num for the month
-let monthNum = getRandomIntInRange(1, 12)
-
 function getMonthName(monthNum) {
     let currentMonth // define variable
     // figure out what text value we should assign to it
     // shouldnt there be a more compact way to do this?
-    switch (key) {
+    switch (monthNum) {
         case 1:
             currentMonth = "January"
             break;
@@ -68,25 +65,42 @@ function getMonthName(monthNum) {
 }
 
 function getFortune(fortNum) {
-    let fortune
+    let fortuneStr
     switch(fortNum) {
         case 1:
-            fortune = 'Finally wake up, discovering that your whole life has been a dream.'
+            fortuneStr = 'finally wake up, discovering that your whole life has been a dream.'
             break;
         case 2:
-            fortune = 'Pass by a hooded stranger as you commute. Their presence will remain with you throughout the day, although you will not realize why until much later.'
+            fortuneStr = 'pass by a hooded stranger as you commute. Their presence will remain with you throughout the day, although you will not realize why until much later.'
             break;
         case 3:
-            fortune = 'Lose a very precious item, only to locate a much more valuable one that same day.'
+            fortuneStr = 'lose a very precious item, only to locate a much more valuable one that same day.'
             break;
         case 4:
-            fortune = 'Lie in bed, frozen by the weight of your inadequacies.'
+            fortuneStr = 'lie in bed, frozen by the weight of your inadequacies.'
             break;
         case 5:
-            fortune = 'Encounter a long-buried memory as you sort through your jumbled belongings.'
+            fortuneStr = 'encounter a long-buried memory as you sort through your jumbled belongings.'
             break;
         default:
-            fortune = "something went wrong lul"
+            fortuneStr = "something went wrong lul"
             break;
     }
+    // return it
+    return fortuneStr
 }
+
+// get a num for the month
+let monthNum = getRandomIntInRange(1, 12)
+// get number for the fortune text
+let fortNum = getRandomIntInRange(1, 5)
+// get a day of the month
+let monthDay = getRandomIntInRange(1, 30) // what if its February though
+
+const monthName = getMonthName(monthNum) // get the string of the month name back
+
+const fortuneFull = getFortune(fortNum) // get a fortune string
+
+const fullText = `On ${monthName} ${monthDay}, you will ${fortuneFull}` // template time
+
+document.querySelector('#crystal').innerText = fullText // put it in the DOM
