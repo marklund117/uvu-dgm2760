58803238b0aea3c9ce912463d97b2919d3eed5c3
Mark Lund <marklund117@gmail.com>
2022-03-15 15:58:03 -0600

    setting up

---
+++ 06nonsense/js/main.js
@@ -0,0 +1,9 @@
+// this is the main JS file for module 6 in DGM 2760 2022 spring, mark lund
+
+// fill text of header h1
+document.querySelector('header > h1').textContent = "Nonsense Story"
+// fill text of header h2
+document.querySelector('header > h2').textContent = "String Manipulation"
+
+
+
