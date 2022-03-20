0304474cf2b9c562f1ff9756cf8c7f52b3b0d53c
Mark Lund <marklund117@gmail.com>
2022-03-20 12:57:29 -0600

    starting setup of module 8

---
+++ 08tomb/js/main.js
@@ -0,0 +1,9 @@
+// this is the main JS file for module 8 in DGM 2760 2022 spring, mark lund
+
+// fill text of header h1
+document.querySelector('header > h1').textContent = "Ancient Tomb"
+// fill text of header h2
+document.querySelector('header > h2').textContent = "Object Literals"
+
+
+
