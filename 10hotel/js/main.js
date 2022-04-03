86cc194484f7f8e726fb197d41f19b9cc5aa1e78
Mark Lund <marklund117@gmail.com>
2022-04-03 14:52:51 -0600

    some initial setup work

---
+++ 10hotel/js/main.js
@@ -0,0 +1,10 @@
+// this is the main JS file for module 8 in DGM 2760 2022 spring, mark lund
+
+// fill text of header h1
+document.querySelector('header > h1').textContent = "Hotel Booking"
+// fill text of header h2
+document.querySelector('header > h2').textContent = "Complete with tiny shampoo bottles"
+
+
+
+
