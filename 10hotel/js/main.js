1dae6bfe7e4917573615d34d66f05675d08953ed
Mark Lund <marklund117@gmail.com>
2022-04-10 18:23:02 -0600

    getting started on JS/JSON

--- 10hotel/js/main.js
+++ 10hotel/js/main.js
@@ -5,6 +5,22 @@ document.querySelector('header > h1').textContent = "Hotel Booking"
 // fill text of header h2
 document.querySelector('header > h2').textContent = "Complete with tiny shampoo bottles"
 
+// time to access the JSON with Fetch
+//async function - this could be made even more general by passing in a URL 
+async function getHotelData() {
+    try {
+        const response = await fetch('../hotel.json') // await the result of the fetch call
+        return await response.json // await converting the response to JSON instead of giving us everything
+    } catch(error) {
+        console.error(error) // send the error to the console if it fails
+    }
+}
+
+let hotelData = [] // empty variable to hold hotel data later
+
+getHotelData().then(data => hotelData = data) // put it into the empty variable (implicit return)
+
+console.log(hotelData)
 
 
 
